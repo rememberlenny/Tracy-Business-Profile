0f79ed2600d70e790e7a17b29e6742409d53f9e6
rememberlenny <lkbgift@gmail.com>
2013-07-30 18:18:57 -0400

    Adding links for top apps

--- wp-content/themes/narga-core-master/module-blog-snip-single.php
+++ wp-content/themes/narga-core-master/module-blog-snip-single.php
@@ -12,11 +12,11 @@
       <li><h4 class="text-right">Top Apps <br>of the week</h4></li>
       <li>
         <img src="<?php bloginfo('url') ?>/wp-content/uploads/2013/06/supper.png" alt="Supper Defender IV" style="max-width:124px">
-        <p><a style="font-size:.7em;" href="">Supper Defender IV</a></p>
+        <p><a style="font-size:.7em;" href="https://play.google.com/store/apps/details?id=com.droidhen.defendersiiii&referrer=utm_source%3Dappbrain%26utm_medium%3Dappbrain_web%26utm_campaign%3Dappbrain_web">Supper Defender IV</a></p>
       </li>
       <li>
         <img src="<?php bloginfo('url') ?>/wp-content/uploads/2013/06/driving.png" alt="Driving School" style="max-width:124px">
-        <p><a style="font-size:.7em;" href="">Driving School</a></p>
+        <p><a style="font-size:.7em;" href="https://play.google.com/store/apps/details?id=com.xianqing.driving3d&referrer=utm_source%3Dappbrain%26utm_medium%3Dappbrain_web%26utm_campaign%3Dappbrain_web">Driving School</a></p>
       </li>
       <li>
         <img src="<?php bloginfo('url') ?>/wp-content/uploads/2013/06/anime.png" alt="Anime Beauty" style="max-width:124px">
@@ -24,7 +24,7 @@
       </li>
       <li>
         <img src="<?php bloginfo('url') ?>/wp-content/uploads/2013/06/hider.png" alt="Application Hider" style="max-width:124px">
-        <p><a style="font-size:.7em;" href="">Application Hider</a></p>
+        <p><a style="font-size:.7em;" href="https://play.google.com/store/apps/details?id=com.mm.security.androidhider1&referrer=utm_source%3Dappbrain%26utm_medium%3Dappbrain_web%26utm_campaign%3Dappbrain_web">Application Hider</a></p>
       </li>
     </ul>
 </div>
