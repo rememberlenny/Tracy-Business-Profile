9400a6f21edfefab59a37a8b61b54930bca6f7a8
rememberlenny <lkbgift@gmail.com>
2013-07-30 13:04:57 -0400

    updating email and background

--- wp-content/themes/narga-core-master/module-blog-snip-single.php
+++ wp-content/themes/narga-core-master/module-blog-snip-single.php
@@ -5,7 +5,7 @@
           $mypost = array( 'post_type' => 'post', 'posts_per_page' => '2' );
           $my_query = new WP_Query( $mypost ); ?>
     <?php while ($my_query->have_posts()) :  $my_query->the_post(); ?>
-    <?php get_template_part('content', 'home-post-single') ?>
+    <?php get_template_part('cosntent', 'home-post-single') ?>
     <?php endwhile; // End the loop ?>
     <hr>
     <ul class="text-center large-block-grid-5">
@@ -20,7 +20,7 @@
       </li>
       <li>
         <img src="<?php bloginfo('url') ?>/wp-content/uploads/2013/06/anime.png" alt="Anime Beauty" style="max-width:124px">
-        <p><a style="font-size:.7em;" href="">Anime Beauty</a></p>
+        <p><a style="font-size:.7em;" href="">Heart Live Wallpaper</a></p>
       </li>
       <li>
         <img src="<?php bloginfo('url') ?>/wp-content/uploads/2013/06/hider.png" alt="Application Hider" style="max-width:124px">
--- wp-content/themes/narga-core-master/module-email-contents.php
+++ wp-content/themes/narga-core-master/module-email-contents.php
@@ -1,7 +1,6 @@
 <div class="row large-10">
   <div class="large-8 column" style="">
-    <h3 class="white text-left subheader fs1em fw100" style="opacity:.8; font-weight: 100 !important;">Stay on top of China's mobile and advertising market.</h3>
-    <p class="white text-left fs08em mb0em" style="opacity:.8">Sign up to receive our weekly e-letter.</p>
+    <h3 class="white text-left subheader fs1em fw100" style="opacity:.8; font-weight: 100 !important; font-size: .9em!important; margin-top: .8em">Stay on top of China's mobile and advertising market. Sign up to receive our weekly e-letter.</p>
   </div>
   <div class="large-4 column pt05em">
   <form action="http://adsyolo.us6.list-manage.com/subscribe/post?u=664675817b&amp;id=4c58b47ded" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate mb0em" target="_blank" novalidate>
